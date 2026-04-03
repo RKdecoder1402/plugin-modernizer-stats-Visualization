22e09e2cd8279ab52338c5f2388d25248df1a05b
Rohit Kushwaha <Kushwaharohit1402@gmail.com>
2026-04-03 13:33:50 +0530

    initial dashboard UI

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h1>Plugin Modernizer Dashboard</h1>
+      <p>Initial prototype in progress...</p>
+    </div>
+  );
+}
+
+export default App;
+
