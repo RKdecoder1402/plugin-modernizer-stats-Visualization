6b64e20540545cba9708a42ad3862f92074fec07
Rohit Kushwaha <Kushwaharohit1402@gmail.com>
2026-04-12 19:46:01 +0530

    add plugin repository URLs to generated dataset

--- scripts/fetch-data.cjs
+++ scripts/fetch-data.cjs
@@ -1,16 +1,27 @@
 const fs = require("fs");
 
-const plugins = [
-  { name: "git", status: "updated" },
-  { name: "docker", status: "needs_migration" },
-  { name: "workflow-job", status: "deprecated" },
-  { name: "kubernetes", status: "needs_migration" },
-  { name: "credentials", status: "updated" }
-];
+async function fetchPlugins() {
+  const res = await fetch(
+    "https://api.github.com/repos/jenkins-infra/metadata-plugin-modernizer/contents/"
+  );
 
-fs.writeFileSync(
-  "src/data/jenkins-plugins.json",
-  JSON.stringify(plugins, null, 2)
-);
+  const data = await res.json();
 
-console.log("plugins data generated");
+  const plugins = data
+    .filter(item => item.type === "dir")
+    .slice(0, 50)
+    .map(p => ({
+  name: p.name,
+  status: "needs_migration",
+  url: `https://github.com/jenkinsci/${p.name}-plugin`
+}));
+
+  fs.writeFileSync(
+    "src/data/jenkins-plugins.json",
+    JSON.stringify(plugins, null, 2)
+  );
+
+  console.log("Real plugin data generated");
+}
+
+fetchPlugins();
